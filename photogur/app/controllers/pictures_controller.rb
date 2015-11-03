665455db3dfa4328992ea1dd296ee659a93100a5
jrdisser <jimdisser@altocirrusapps.com>
2015-11-02 21:00:54 -0500

    added caption field removed album field

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -70,6 +70,6 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:title, :album, :url)
+      params.require(:picture).permit(:title, :caption, :url) #JRD110215
     end
 end
