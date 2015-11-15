b9a633257203b777e0a60e8c0bbdcb671e3c3a2b
Keith Tang <keithsktang@gmail.com>
2015-11-15 16:18:07 -0500

    restore files final

--- PhotoMagic/app/controllers/pictures_controller.rb
+++ PhotoMagic/app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  http_basic_authenticate_with name: "PhotoMagic", password: "secret", except: [:index, :show]
+  # http_basic_authenticate_with name: "PhotoMagic", password: "secret", except: [:index, :show]
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
 
